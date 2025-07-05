3655fa980c7c2c4880c4fbe59995d618183e3c46
62062-tanesh <tanesh8409@gmail.com>
2025-07-06 01:05:59 +0530

    Added New Programs

---
+++ IF Else/src/53 TernaryOperator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class FindTheMinimumOfTwo {
+        public static void main(String [] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter The First Number: ");
+            int a = sc.nextInt();
+            System.out.print("Enter The Second NUmber: ");
+            int b = sc.nextInt();
+            int result = findMin(a,b);
+            System.out.println("The Min is: "+result);
+        }
+        public static int findMin(int a, int b){
+            return a<b ? a:b;
+//            if(a<b){
+//                return a;
+//            }
+//            else{
+//                return b;
+//            }
+        }
+}
