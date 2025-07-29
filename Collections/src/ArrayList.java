6b63c76c8134339b158145825094d464db7f46cf
62062-tanesh <tanesh8409@gmail.com>
2025-07-30 02:08:03 +0530

    Added Collection & Generics Questions

--- Collections/src/ArrayList.java
+++ Collections/src/ArrayList.java
@@ -1,5 +1,12 @@
 public class ArrayList {
-    public static void main(String[] args) {
-        ArrayList
+    public static void main(String[] args){
+        System.out.println(AddStrings("Tanesh","Tum","KahaHO"));
+    }
+    public static String AddStrings(String... str){
+        String result = "";
+        for(String i : str){
+            result += i;
+        }
+        return result;
     }
 }
