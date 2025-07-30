d6ce1a4ab0186dca15d03d52b22dea98394d1d1c
62062-tanesh <tanesh8409@gmail.com>
2025-07-30 22:22:04 +0530

    Update The Programs

--- Collections/src/ArrayList.java
+++ Collections/src/ArrayList.java
@@ -1,11 +1,12 @@
 public class ArrayList {
     public static void main(String[] args){
-        System.out.println(AddStrings("Tanesh","Tum","KahaHO"));
+        System.out.println(AddStrings("Tanesh","Tum","Kaha HO"));
     }
     public static String AddStrings(String... str){
         String result = "";
         for(String i : str){
             result += i;
+            result += " ";
         }
         return result;
     }
