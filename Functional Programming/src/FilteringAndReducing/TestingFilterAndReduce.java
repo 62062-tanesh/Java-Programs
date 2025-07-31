3d51b230e6d2216126c19a1fafa6dc829e9c3df2
62062-tanesh <tanesh8409@gmail.com>
2025-08-01 02:07:02 +0530

    Added Functional Programming Questions

---
+++ Functional Programming/src/FilteringAndReducing/TestingFilterAndReduce.java
@@ -0,0 +1,25 @@
+package FilteringAndReducing;
+
+import javax.crypto.spec.PSource;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class TestingFilterAndReduce {
+    public static void main(String[] args) {
+        List<String> fruits = List.of("Apple","Banana","Mango","Papaya");
+        System.out.println(fruits.size());
+        for(String i : fruits){
+            System.out.println(i);
+        }
+        System.out.println("Printing with Streams");
+        fruits.stream().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+        System.out.println("With Lambda Expression");
+        fruits.stream().filter(fruite -> fruite.endsWith("e")).forEach(fruit -> System.out.println(fruit));
+    }
+
+}
